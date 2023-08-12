9dbd83bc133cc8b6317330c34f7aef54a752bba7
troyedwardsjr <troyedwardsjr@gmail.com>
2023-08-12 15:09:03 -0400

    update build.rs

--- rust/flecs_core/build.rs
+++ rust/flecs_core/build.rs
@@ -1,6 +1,8 @@
 fn main() {
     // Tell cargo to invalidate the built crate whenever the sources change
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=flecs.h");
+    println!("cargo:rerun-if-changed=flecs.c");
 
     // Get rid of the warning about unused command line arguments from emcc
     std::env::set_var("CFLAGS", "-Wno-unused-command-line-argument");
