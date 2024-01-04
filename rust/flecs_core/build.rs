95d1b15575bbb4dc994097a311e3bf6f2bba2a44
troyedwardsjr <troyedwardsjr@gmail.com>
2024-01-03 19:09:07 -0500

    change to contains emscripten flag

--- rust/flecs_core/build.rs
+++ rust/flecs_core/build.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("cargo:rerun-if-changed=flecs.c");
 
     let target = std::env::var("TARGET").unwrap();
-        if !target.contains("emscripten") {
+    if target.contains("emscripten") {
         // Get rid of the warning about unused command line arguments from emcc
         std::env::set_var("CFLAGS", "-Wno-unused-command-line-argument");
     };
